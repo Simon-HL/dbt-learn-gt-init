146fedcdc45549d7d34117cce140aeb5ba85db50
Simon Holthe Hansen <shh@sonett.no>
2026-01-06 14:15:42 +0100

    feat: add models for orders and stg_payments

---
+++ models/marts/finance/fct_orders.sql
@@ -0,0 +1,21 @@
+with orders as (
+    select * from {{ ref('stg_jaffle_shop__orders')}}
+),
+
+payments as (
+    select * from {{ ref('stg_stripe__payments')}}
+),
+
+final as (
+
+    select
+        orders.order_id,
+        orders.customer_id,
+        payments.amount,
+    from orders
+    left join payments using (order_id)
+    where payments.status = 'success'
+
+)
+
+select * from final
