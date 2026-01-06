7d86ba7c01f1795d639562d0c2aab402a396d7a4
Simon Holthe Hansen <shh@sonett.no>
2026-01-06 15:07:56 +0100

    refact: use fct_orders to get lifetime value in customer dimension instead of stg_payment

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -3,46 +3,23 @@ with customers as (
 ),
 
 orders as (
-    select * from {{ ref('stg_jaffle_shop__orders')}}
-),
-
-payments as (
-    select * from {{ ref('stg_stripe__payments')}}
+    select * from {{ ref('fct_orders')}}
 ),
 
 
 customer_orders as (
-
     select
         customer_id,
-
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
-
+        count(order_id) as number_of_orders,
+        sum(amount) as lifetime_value
     from orders
-
     group by 1
-
-),
-
-customer_payments as (
-
-    select
-        customer_id,
-
-        sum(amount) as lifetime_value,
-
-    from payments
-    where status = 'success'
-
-    group by 1
-
 ),
 
 
 final as (
-
     select
         customers.customer_id,
         customers.first_name,
@@ -50,13 +27,9 @@ final as (
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
-        customer_payments.lifetime_value as lifetime_value
-
+        customer_orders.lifetime_value
     from customers
-
     left join customer_orders using (customer_id)
-    left join customer_payments using (customer_id)
-
 )
 
 select * from final
