bd4eb5e7fd4eae7163343c9d31f5292d730e8daa
Simon Holthe Hansen <shh@sonett.no>
2026-01-06 14:22:27 +0100

    feat: add lifetime_value prop to customers

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -6,6 +6,11 @@ orders as (
     select * from {{ ref('stg_jaffle_shop__orders')}}
 ),
 
+payments as (
+    select * from {{ ref('stg_stripe__payments')}}
+),
+
+
 customer_orders as (
 
     select
@@ -21,6 +26,20 @@ customer_orders as (
 
 ),
 
+customer_payments as (
+
+    select
+        customer_id,
+
+        sum(amount) as lifetime_value,
+
+    from payments
+    where status = 'success'
+
+    group by 1
+
+),
+
 
 final as (
 
@@ -30,11 +49,13 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        customer_payments.lifetime_value as lifetime_value
 
     from customers
 
     left join customer_orders using (customer_id)
+    left join customer_payments using (customer_id)
 
 )
 
